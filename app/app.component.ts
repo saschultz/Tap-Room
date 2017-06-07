199262491743328f70a0b01a5b37c53861716bb9
Ben Williams and Sara Schultz <ben@>
2017-06-07 11:57:03 -0700

    add functionality to edit a keg

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,8 +9,21 @@ import { Component } from '@angular/core';
   <div class="container">
     <h2>Kegs</h2>
     <ul>
-      <li *ngFor="let currentKeg of kegs"><b>{{currentKeg.name}}</b> -<em>{{currentKeg.brand}}</em><br>alcohol content: {{currentKeg.alcoholContent}}%  \${{currentKeg.price}} per pint</li>
+      <li *ngFor="let currentKeg of kegs"><b>{{currentKeg.name}}</b> -<em>{{currentKeg.brand}}</em><br>alcohol content: {{currentKeg.alcoholContent}}%  \${{currentKeg.price}} per pint <button (click)="editKeg(currentKeg)">Edit Keg</button></li>
     </ul>
+    <div *ngIf="selectedKeg">
+      <h3>{{selectedKeg.name}}</h3>
+      <h3>Edit Keg</h3>
+      <label>Enter Keg Name</label>
+      <input [(ngModel)]="selectedKeg.name">
+      <label>Enter Keg Brand</label>
+      <input [(ngModel)]="selectedKeg.brand">
+      <label>Enter Keg Alcohol Content</label>
+      <input [(ngModel)]="selectedKeg.alcoholContent">
+      <label>Enter Keg Price</label>
+      <input [(ngModel)]="selectedKeg.price">
+      <button (click)="finishedEditing()">Done</button>
+    </div>
   </div>
   `
 })
@@ -22,6 +35,15 @@ export class AppComponent {
     new Keg('Hip Hoppy IPA', 'Splitting Hares', 7, 5),
     new Keg('Pitch Noir', 'Busta Limes', 5, 4)
   ];
+  selectedKeg = null;
+
+  editKeg(clickedKeg) {
+    this.selectedKeg = clickedKeg;
+  }
+
+  finishedEditing() {
+    this.selectedKeg = null;
+  }
 }
 
 export class Keg {
