16c1dc1f966cc75580daabb084143182679224b9
Ben Williams and Sara Schultz <sara@>
2017-06-08 15:03:50 -0700

    add other keg functionality to keg-list component

--- app/keg-list.component.ts
+++ app/keg-list.component.ts
@@ -5,15 +5,12 @@ import { Keg } from './keg.model';
   selector: 'keg-list',
   template: `
   <ul>
-  <li *ngFor="let currentKeg of childKegList"><b>{{currentKeg.name}}</b> -<em>{{currentKeg.brand}}</em><br>alcohol content: <span> {{currentKeg.alcoholContent}}% </span>  \${{currentKeg.price}} per pint <button (click)="editKegClicked(currentKeg)">Edit Keg</button> {{currentKeg.pints}} pints
-  </li>
+    <li [class]="brandColor(currentKeg)" *ngFor="let currentKeg of childKegList"><b>{{currentKeg.name}}</b> -<em>{{currentKeg.brand}}</em><br>alcohol content: <span [class]="alcBold(currentKeg)"> {{currentKeg.alcoholContent}}% </span>  \${{currentKeg.price}} per pint <button (click)="editKegClicked(currentKeg)">Edit Keg</button> {{currentKeg.pints}} pints <button (click)="sellPint(currentKeg)">Sold Pint</button> <button (click)="replaceKeg(currentKeg)">Replace this keg</button>
+    </li>
   </ul>
   `
 })
-// [class]="alcBold(currentKeg)" GOES INSIDE THE SPAN
-// [class]="brandColor(currentKeg)"
-// <button (click)="replaceKeg(currentKeg)">Replace this keg</button>
-// <button (click)="sellPint(currentKeg)">Sold Pint</button>
+
 
 export class KegListComponent {
   @Input() childKegList: Keg[];
@@ -23,34 +20,34 @@ export class KegListComponent {
     this.clickSender.emit(kegToEdit);
   }
 
-  // sellPint(currentKeg) {
-  //   if (currentKeg.pints > 0) {
-  //     currentKeg.pints -= 1;
-  //     if (currentKeg.pints === 10) {
-  //       alert('Warning! Running low on this keg! Life is finite.');
-  //     }
-  //   }
-  // }
-  //
-  // replaceKeg(currentKeg) {
-  //   currentKeg.pints = 124;
-  // }
-  //
-  // brandColor(currentKeg) {
-  //   if (currentKeg.brand === 'Forever Alone') {
-  //     return 'bg-danger';
-  //   } else if (currentKeg.brand === 'Liphes Gud') {
-  //     return 'bg-info';
-  //   } else if (currentKeg.brand === 'Splitting Hares') {
-  //     return 'bg-warning';
-  //   } else {
-  //     return 'bg-success';
-  //   }
-  // }
-  //
-  // alcBold(currentKeg) {
-  //   if (currentKeg.alcoholContent > 6) {
-  //     return 'badge';
-  //   }
-  // }
+  sellPint(currentKeg) {
+    if (currentKeg.pints > 0) {
+      currentKeg.pints -= 1;
+      if (currentKeg.pints === 10) {
+        alert('Warning! Running low on this keg! Life is finite.');
+      }
+    }
+  }
+
+  replaceKeg(currentKeg) {
+    currentKeg.pints = 124;
+  }
+
+  brandColor(currentKeg) {
+    if (currentKeg.brand === 'Forever Alone') {
+      return 'bg-danger';
+    } else if (currentKeg.brand === 'Liphes Gud') {
+      return 'bg-info';
+    } else if (currentKeg.brand === 'Splitting Hares') {
+      return 'bg-warning';
+    } else {
+      return 'bg-success';
+    }
+  }
+
+  alcBold(currentKeg) {
+    if (currentKeg.alcoholContent > 6) {
+      return 'badge';
+    }
+  }
 }
